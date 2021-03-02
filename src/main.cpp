bd132b69b2dc856afa305e567a267cdc621a56ae
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-02 15:49:14 -0300

    why tf its not working??

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,16 @@
 std::queue<std::string> msgs;
 
 int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 
-    window win = window("Klonoa Dimensions", 720, 480);
+	window win = window("Klonoa Dimensions", 720, 480);
 
-    character klonoa = character(vec2f(0, 0), vec2f(64, 64), vec2f(0, 0), vec2f(100, GRAVITY));
-    image k = image(win, "res/sprites/klonoa/character/Klonoa Idle Right.png", { 0, 0, 16, 16 }, { 0, 0, 0, 0 } );
-    area_manager area_man = area_manager();
+	character klonoa = character(vec2f(0, 0), vec2f(64, 64), vec2f(0, 0), vec2f(100, GRAVITY));
+	image k = image(win, "res/sprites/klonoa/character/Klonoa Idle Right.png", { 0, 0, 16, 16 }, { 100, 0, 100, 100 } );
+	area_manager area_man = area_manager();
 
-    area_man.register_area(vec2f(100, 100), vec2f(50, 50));
+    area_man.register_area(vec2f(100, 100), vec2f(150, 150), &k);
 
 
 
