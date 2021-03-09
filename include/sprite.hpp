97ee5bffdb46e480ee42bee8fc569208665bd9cc
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-09 13:29:15 -0300

    Added getters for "sprite" class attributes

--- include/sprite.hpp
+++ include/sprite.hpp
@@ -14,9 +14,10 @@ private:
 
 public:
     sprite(const window& win, const char* path, const SDL_Rect src, const SDL_Rect des, int index = 0, int x_advance = 0, int y_advance = 0) :
-		image(win, path, src, des), index(index), x_advance(x_advance), y_advance(y_advance) {
-			;
+		image(win, path, src, des), index(index), x_advance(x_advance), y_advance(y_advance) { }
 
-	}
+	int get_index() const { return this->index; }
+	int get_x_advance() const { return this->x_advance; }
+	int get_y_advance() const { return this->y_advance; }
 
 };
