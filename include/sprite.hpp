727d33ac3f16a4e555fe22bd3586d5e4ecd677a2
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-09 13:31:46 -0300

    Added setters for "sprite" class attributes

--- include/sprite.hpp
+++ include/sprite.hpp
@@ -13,8 +13,11 @@ private:
         x_advance, y_advance;
 
 public:
-    sprite(const window& win, const char* path, const SDL_Rect src, const SDL_Rect des, int index = 0, int x_advance = 0, int y_advance = 0) :
+    sprite(const window& win, const char* path, const SDL_Rect src, const SDL_Rect des, const int index = 0, const int x_advance = 0, const int y_advance = 0) :
 		image(win, path, src, des), index(index), x_advance(x_advance), y_advance(y_advance) { }
+
+	void set_x_advance(const int x_advance) { this->x_advance = x_advance; }
+	void set_y_advance(const int y_advance) { this->y_advance = y_advance; }
 
 	int get_index() const { return this->index; }
 	int get_x_advance() const { return this->x_advance; }
