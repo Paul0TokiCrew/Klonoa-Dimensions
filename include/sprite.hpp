39b760427bd351de4a1f020390e1830714f0bed8
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-09 13:26:14 -0300

    Updated "sprite" class

--- include/sprite.hpp
+++ include/sprite.hpp
@@ -7,4 +7,16 @@
 
 
 
-class sprite : public image { };
+class sprite : public image {
+private:
+    int index,
+        x_advance, y_advance;
+
+public:
+    sprite(const window& win, const char* path, const SDL_Rect src, const SDL_Rect des, int index = 0, int x_advance = 0, int y_advance = 0) :
+		image(win, path, src, des), index(index), x_advance(x_advance), y_advance(y_advance) {
+			;
+
+	}
+
+};
