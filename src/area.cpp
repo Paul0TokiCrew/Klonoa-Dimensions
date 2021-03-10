d05640de14a5f4c18a39e4617e64e71db79dc97f
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-10 14:45:28 -0300

    updated change_areas_pos

--- src/area.cpp
+++ src/area.cpp
@@ -112,7 +112,9 @@ void area_manager::change_areas_pos(const vec2f mod) {
 	for (auto i : this->collision_areas) {
 
 		image*& img = std::get<2>(i);
-		img->change_pos(img->get_old_des().x - std::ceil(mod.x), img->get_old_des().y - std::ceil(mod.y));
+
+		if (img != nullptr)
+			img->change_pos(img->get_old_des().x - std::ceil(mod.x), img->get_old_des().y - std::ceil(mod.y));
 
 	}
 
