082a9bc815911aa20e7b4efed93a4c14136630b6
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-16 15:12:52 -0300

    Updated "game::init()" method

--- src/game.cpp
+++ src/game.cpp
@@ -10,11 +10,13 @@ float get_current_time() { return SDL_GetTicks() / 1000.0f; }
 
 bool game::init() {
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		msgs.push_back("ERROR: could not initialize SDL library");
 		return false;
 		
 	}
 
 	if (IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) != (IMG_INIT_PNG | IMG_INIT_JPG)) {
+		msgs.push_back("ERROR: could not initialize SDL_image library");
 		return false;
 		
 	}
