70b96bd650e42ffad89a526513cff65eecd3ad32
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-04-01 10:48:39 -0300

    Added "entity_manager" class

--- include/ECS.hpp
+++ include/ECS.hpp
@@ -3,16 +3,20 @@
 
 
 #include <cstdint>
+#include <bitset>
+#include <array>
+#include <queue>
 
 
 
-#define MAX_ENTITES 1000
+#define MAX_ENTITIES 1000
 #define MAX_COMPONENTS 500
 
 
 
 using entity = std::uint16_t;
 using component_id = std::uint16_t;
+using signature = std::bitset<MAX_COMPONENTS>;
 
 
 
@@ -26,3 +30,22 @@ inline component_id get_id() {
 	static component_id id = get_new_id();
 	return id;
 }
+
+
+
+class entity_manager {
+private:
+	std::uint32_t entity_count;
+	std::queue<entity> avaible_entities;
+
+	std::array<signature, MAX_ENTITIES> signs;
+
+public:
+	entity_manager() :
+		entity_count(0), avaible_entities { }, signs { } {
+		for (entity i = 0; i < MAX_ENTITIES; ++i)
+			this->avaible_entities.push(i);
+
+	}
+
+};
