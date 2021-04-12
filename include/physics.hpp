96cad3bc57d089aec386f14f75c26163424554d6
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-04-12 11:14:47 -0300

    Added "physics::update()" method

--- include/physics.hpp
+++ include/physics.hpp
@@ -1,12 +1,25 @@
+#pragma once
+
+
+
+#include <log.hpp>
 #include <ECS.hpp>
+#include <vec2f.hpp>
+#include <area.hpp>
+
+
 
 struct c_position { vec2f xy, wh; };
 
 struct c_movement { vec2f dir, vel, goal_vel; };
 
+
+
 class physics : public system {
 public:
 	physics() { }
 	~physics() { }
+
+	void update(const float delta_time, const area_manager& area_man);
 
 };
---
+++ src/physics.cpp
@@ -0,0 +1,93 @@
+#include <physics.hpp>
+
+
+
+void physics::update(const float delta_time, const area_manager& area_man) {
+
+	for (const auto& ent : this->ents) {
+
+		c_position position = crd.get_comp<c_position>(ent);
+		c_movement movement = crd.get_comp<c_movement>(ent);
+
+
+
+		vec2f xy2 = vec2f(position.xy + position.wh),
+			move = vec2f(movement.vel * vec2f(delta_time)),
+			diff = vec2f(0, 0);
+
+		const float fric = area_man.get_fric(position.xy, xy2);
+
+
+
+		if (area_man.check_up_collision(position.xy + move, xy2 + move, &diff)) {
+			msgs.push("up collision detected");
+
+			if (movement.vel.y < 0) {
+				movement.vel.y = 0;
+				move.y += diff.y;
+
+			}
+
+		}
+
+		if (area_man.check_down_collision(position.xy + move, xy2 + move, &diff)) {
+			msgs.push("down collision detected");
+
+			if (movement.vel.y > 0) {
+				movement.vel.y = 0;
+				move.y += diff.y;
+
+			}
+
+		}
+
+		if (area_man.check_right_collision(position.xy + move, xy2 + move, &diff)) {
+			msgs.push("right collision detected");
+
+			if (movement.vel.x > 0) {
+				movement.vel.x = 0;
+				move.x += diff.x;
+
+			}
+
+		}
+
+		if (area_man.check_left_collision(position.xy + move, xy2 + move, &diff)) {
+			msgs.push("left collision detected");
+
+			if (movement.vel.x < 0) {
+				movement.vel.x = 0;
+				move.x += diff.x;
+
+			}
+
+		}
+
+
+
+		if (std::ceil(movement.vel.x))
+			position.xy.x += move.x;
+
+		if (std::ceil(movement.vel.y))
+			position.xy.y += move.y;
+
+
+
+		if (std::ceil(movement.vel.x) < std::ceil(movement.goal_vel.x))
+			movement.vel.x += delta_time * fric;
+
+		else if (std::ceil(movement.vel.x) > std::ceil(movement.goal_vel.x))
+			movement.vel.x -= delta_time * fric;
+
+
+
+		if (std::ceil(movement.vel.y) < std::ceil(movement.goal_vel.y))
+			movement.vel.y += delta_time * fric;
+
+		else if (std::ceil(movement.vel.y) > std::ceil(movement.goal_vel.y))
+			movement.vel.y -= delta_time * fric;
+
+
+	}
+
+}
