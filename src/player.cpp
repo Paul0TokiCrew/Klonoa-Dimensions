32038d698625ddc59667c62c2e635b24e20aeb88
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-04-19 19:51:22 -0300

    im tired rn

--- src/player.cpp
+++ src/player.cpp
@@ -2,44 +2,56 @@
 
 
 
-void player::update(const Uint8* key, const area_manager& area_man) const {
+void player::update(const Uint8* key, const area_manager& area_man) {
 
-	for (auto i : this->ents) {
+	for (const auto& ent : this->ents) {
 
-		/*if (!area_man.check_down_collision(this->xy, vec2f(this->xy - this->wh))) {
-			this->goal_vel.y = this->max_vel.y;
-
-			if (std::ceil(this->vel.y) >= 0)
-				this->dir.y = 1;
-
-		} else
-			this->goal_vel.y = 0;
+		c_player_keys& player_keys = crd.get_comp<c_player_keys>(ent);
+		c_position& position = crd.get_comp<c_position>(ent);
+		c_movement& movement = crd.get_comp<c_movement>(ent);
 
 
 
+		const unsigned int up = player_keys.keys[UP],
+			right = player_keys.keys[RIGHT],
+			left = player_keys.keys[LEFT];
 
-		if (key[SDL_SCANCODE_RIGHT]) {
-			this->goal_vel.x = this->max_vel.x;
-			this->dir.x = 1;
+		const vec2f xy2 = vec2f(position.xy - position.wh);
 
-		} else if (key[SDL_SCANCODE_LEFT]) {
-			this->goal_vel.x = -(this->max_vel.x);
-			this->dir.x = -1;
+		if (!area_man.check_down_collision(position.xy, xy2)) {
+			movement.goal_vel.y = movement.max_vel.y;
+
+			if (std::ceil(movement.vel.y) >= 0)
+				movement.dir.y = 1;
 
 		} else
-			this->goal_vel.x = 0;
+			movement.goal_vel.y = 0;
+
+
+
+
+		if (key[right]) {
+			movement.goal_vel.x = movement.max_vel.x;
+			movement.dir.x = 1;
+
+		} else if (key[left]) {
+			movement.goal_vel.x = -(movement.max_vel.x);
+			movement.dir.x = -1;
+
+		} else
+			movement.goal_vel.x = 0;
 
 
 		
-		if (key[SDL_SCANCODE_SPACE]) {
-			this->vel.y = -100;
-			this->dir.y = -1;
+		if (key[up]) {
+			movement.vel.y = -100;
+			movement.dir.y = -1;
 
 		}
 
 
 		if (this->counter)
-			--counter;*/
+			--counter;
 
 	}
 
